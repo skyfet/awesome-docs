2db4781f62d7a7fbe48c27ed72b87e002905c1f1
archi d bill <skryfet@ya.ru>
2025-04-15 11:01:30 +0300

    fix: configure basename for prod and dev

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,9 +4,13 @@ import DocumentsPage from "../features/documents/DocumentsPage";
 import RequireAuth from "../features/auth/RequireAuth";
 import CssBaseline from "@mui/material/CssBaseline";
 
+const isProd = import.meta.env.MODE === "production";
+const basename = isProd ? "/awesome-docs" : "/";
+
 function App() {
   return (
     <BrowserRouter
+      basename={basename}
       future={{
         v7_relativeSplatPath: true,
         v7_startTransition: true,
